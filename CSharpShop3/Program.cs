47857179f8e0d2793f51f970a5d6b79dc288673f
VolpinAndrea <andvolpin19@gmail.com>
2022-12-06 16:22:40 +0100

    Metodo statico creaBottiglia nel main

--- CSharpShop3/Program.cs
+++ CSharpShop3/Program.cs
@@ -1,25 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using CSharpShop3;
-bool controllo = true;
-while (controllo)
+
+
+Acqua uno = CreaBottiglia();
+
+static Acqua? CreaBottiglia()
 {
-    try
+    //bool controllo = true;
+    while (true) // controllo
     {
-        Console.WriteLine("Inserisci la capienza della bottiglia");
-        double capienzaUtente = double.Parse(Console.ReadLine());
-        Console.WriteLine("Inserisci il pH dell'acqua");
-        double phUtente = double.Parse(Console.ReadLine());
+        try
+        {
+            Console.WriteLine("Inserisci la capienza della bottiglia");
+            double capienzaUtente = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci il pH dell'acqua");
+            double phUtente = double.Parse(Console.ReadLine());
 
-        Acqua bottle = new("H2o", "Frizzante", 2.00, 0.3, capienzaUtente, phUtente, "Monte");
-        controllo= false;
-        Console.WriteLine(bottle.ToString());
+            Acqua bottle = new("H2o", "Frizzante", 2.00, 0.3, capienzaUtente, phUtente, "Monte");
+            //controllo = false;
+            Console.WriteLine(bottle.ToString());
 
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e.Message);
-        Console.WriteLine("Reinserisci il pH tra 0 e 10");
+            return bottle;
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Reinserisci il pH tra 0 e 10");
+        }
+        //return null;
+
     }
 }
 
+ 
